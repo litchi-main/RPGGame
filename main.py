1bdebbef43928b9238c198cfc2d93afee12a3632
Daniel Borkovski <113119069+litchi-main@users.noreply.github.com>
2025-02-24 10:26:11 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,8 @@
+from games import *
+
+
+game = NuRPG()
+
+game.play_game(alpha_beta_player, alpha_beta_player) # minmax_player , random_player query_player  alpha_beta_player expect_minmax_player
+
+
